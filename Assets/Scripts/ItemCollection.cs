ccebed2470ecc54034c5d1dfdda2baf90f88ba79
Rene Carballo <rcarballo2222@gmail.com>
2020-12-17 15:02:18 -0500

    Initial Commit

---
+++ Assets/Scripts/ItemCollection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollection : MonoBehaviour
+{
+    public GameObject ScoreBox;
+    public AudioSource collectSound;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter() {
+        GlobalScores.num_collectibles += 1;
+        collectSound.Play();
+        Destroy(gameObject);
+    }
+}
